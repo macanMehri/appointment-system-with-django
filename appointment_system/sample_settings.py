a2e0aa2efb6cce395f714a795c195c7cc2dfffba
Macan Mehri <macanmehri@gmail.com>
2024-04-22 17:36:50 +0330

    Initialized the project

---
+++ appointment_system/sample_settings.py
@@ -0,0 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-@kp^_tol)p(+-^r5y@4!e)@=i&kb_ejmi&$7kc_*709d1(ewjq'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
