2c47e762c12e48b4c9d66d4fb27e4fb96bfdcc09
Macan Mehri <macanmehri@gmail.com>
2024-06-23 15:41:56 +0330

    Added a viewset for apis

--- patient/urls.py
+++ patient/urls.py
@@ -1,9 +1,53 @@
-from django.urls import path
-from .views import landing_page, login_page, sign_up_page
+from django.urls import path, include
+from .views import (
+    PatientViewSet, ReportViewSet, PatientsReportsViewSet, MedicineViewSet, ServiceViewSet, ReservationViewSet,
+    AccountViewSet, AvailableTimesViewSet, UsersSuggestionViewSet, DoctorViewSet, DoctorsServicesViewSet,
+)
+from rest_framework import routers
 
+doctor_router = routers.DefaultRouter()
+doctor_router.register('', DoctorViewSet)
+
+patient_router = routers.DefaultRouter()
+patient_router.register('', PatientViewSet)
+
+report_router = routers.DefaultRouter()
+report_router.register('', ReportViewSet)
+
+patients_report_router = routers.DefaultRouter()
+patients_report_router.register('', PatientsReportsViewSet)
+
+medicine_report_router = routers.DefaultRouter()
+medicine_report_router.register('', MedicineViewSet)
+
+service_report_router = routers.DefaultRouter()
+service_report_router.register('', ServiceViewSet)
+
+reservation_report_router = routers.DefaultRouter()
+reservation_report_router.register('', ReservationViewSet)
+
+account_report_router = routers.DefaultRouter()
+account_report_router.register('', AccountViewSet)
+
+available_time_report_router = routers.DefaultRouter()
+available_time_report_router.register('', AvailableTimesViewSet)
+
+user_suggestion_report_router = routers.DefaultRouter()
+user_suggestion_report_router.register('', UsersSuggestionViewSet)
+
+doctor_services_report_router = routers.DefaultRouter()
+doctor_services_report_router.register('', DoctorsServicesViewSet)
 
 urlpatterns = [
-    path('', landing_page),
-    path('login/', login_page),
-    path('signup/', sign_up_page),
+    path('doctor_api/', include(doctor_router.urls,)),
+    path('patient_api/', include(patient_router.urls,)),
+    path('report_api/', include(report_router.urls,)),
+    path('patients_report_api/', include(patients_report_router.urls,)),
+    path('medicine_report_api/', include(medicine_report_router.urls,)),
+    path('service_report_api/', include(service_report_router.urls,)),
+    path('reservation_report_api/', include(reservation_report_router.urls,)),
+    path('account_report_api/', include(account_report_router.urls,)),
+    path('available_time_report_api/', include(available_time_report_router.urls,)),
+    path('user_suggestion_report_api/', include(user_suggestion_report_router.urls,)),
+    path('doctor_services_report_api/', include(doctor_services_report_router.urls,)),
 ]
--- patient/views.py
+++ patient/views.py
@@ -1,15 +1,77 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
+from .serializer import (
+    PatientSerializer, ReportSerializer, PatientsReportsSerializer, MedicineSerializer, ServiceSerializer,
+    ReservationSerializer, AccountSerializer, AvailableTimesSerializer, UsersSuggestionSerializer,
+    DoctorSerializer, DoctorsServicesSerializer,
+)
+from .models import (
+    Patient, Report, PatientsReports, Medicine, Service, Reservation, Account, AvailableTimes, UsersSuggestion,
+    Doctor, DoctorsServices,
+)
 
 
-def landing_page(request):
-    return render(request, 'landingpage.html')
+class DoctorViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = DoctorSerializer
+    queryset = Doctor.objects.filter(is_active=True,).order_by('pk')
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
 
 
-def login_page(request):
-    return render(request, 'loginpage.html')
+class PatientViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = PatientSerializer
+    queryset = Patient.objects.filter(is_active=True,).order_by('pk')
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
 
 
-def sign_up_page(request):
-    return render(request, 'signuppage.html')
+class ReportViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ReportSerializer
+    queryset = Report.objects.filter(is_active=True,).order_by('pk')
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
 
+
+class PatientsReportsViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = PatientsReportsSerializer
+    queryset = PatientsReports.objects.filter(is_active=True,).order_by('pk')
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+
+
+class MedicineViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = MedicineSerializer
+    queryset = Medicine.objects.filter(is_active=True,).order_by('pk')
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+
+
+class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ServiceSerializer
+    queryset = Service.objects.filter(is_active=True,).order_by('pk')
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+
+
+class ReservationViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ReservationSerializer
+    queryset = Reservation.objects.filter(is_active=True,).order_by('pk')
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+
+
+class AccountViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = AccountSerializer
+    queryset = Account.objects.filter(is_active=True,).order_by('pk')
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+
+
+class AvailableTimesViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = AvailableTimesSerializer
+    queryset = AvailableTimes.objects.filter(is_active=True,).order_by('pk')
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+
+
+class UsersSuggestionViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = UsersSuggestionSerializer
+    queryset = UsersSuggestion.objects.filter(is_active=True,).order_by('pk')
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+
+
+class DoctorsServicesViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = DoctorsServicesSerializer
+    queryset = DoctorsServices.objects.filter(is_active=True,).order_by('pk')
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
